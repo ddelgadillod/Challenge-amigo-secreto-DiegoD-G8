cb748bfd3d6d14c0230330515c0db2eb86597651
ddelgadillod <diegodelgadilloduran@gmail.com>
2025-02-26 21:40:51 -0500

    Edita index.html

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let limiteAmigos = 5;
+let amigos = [];
+
+
+function agregarAmigo() {
+    let amigoIngresado = document.getElementById('amigo').value
+    if(!amigoIngresado){
+        alert("Ingrese un nombre de amigo valido");
+        return;
+    }
+    amigos.push(amigoIngresado);
+    console.log(amigos);
+    limpiarCaja();
+    renderizarAmigos();
+}
+
+function limpiarCaja(){
+    document.querySelector('#amigo').value = '';
+    return;
+}
+
+function renderizarAmigos() {
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+    for(let j=0; j < amigos.length; j++){
+        let item = document.createElement("li");
+        item.textContent = amigos[j];
+        listaAmigos.appendChild(item);
+    }
+}
+
+function sortearAmigo(){
+    if (amigos.length == 0){
+        alert("Ingrese una lista de amigos")
+        return;
+    }
+    let amigoSecreto = amigos[Math.floor(Math.random() * amigos.length)];
+
+    let listaAmigoSecreto = document.getElementById("resultado");
+    listaAmigoSecreto.innerHTML = `El amigo secreto es ${amigoSecreto}`;
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+}
